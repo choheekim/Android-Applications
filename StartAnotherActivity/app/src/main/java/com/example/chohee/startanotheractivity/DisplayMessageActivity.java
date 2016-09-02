9307d8801d63b7acde95437fef7ff547fa8e8ac4
Chohee Kim <kch5559@gmail.com>
2016-09-02 15:27:14 -0700

    Implement an Options menu

--- StartAnotherActivity/app/src/main/java/com/example/chohee/startanotheractivity/DisplayMessageActivity.java
+++ StartAnotherActivity/app/src/main/java/com/example/chohee/startanotheractivity/DisplayMessageActivity.java
@@ -3,6 +3,9 @@ package com.example.chohee.startanotheractivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
@@ -16,8 +19,33 @@ public class DisplayMessageActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = (TextView) findViewById(R.id.display_message);
         textView.setText(message);
+        textView.setTextSize(20);
 
 
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.options_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+
+        int id = menuItem.getItemId();
+
+        if(id == R.id.go_back) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(menuItem);
     }
 
 }
--- StartAnotherActivity/app/src/main/java/com/example/chohee/startanotheractivity/MainActivity.java
+++ StartAnotherActivity/app/src/main/java/com/example/chohee/startanotheractivity/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.chohee.startanotheractivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.EditText;
 
@@ -15,6 +17,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.options_menu, menu);
+
+        return true;
+    }
+
+
 
     public void sendMessage(View view) {
 
